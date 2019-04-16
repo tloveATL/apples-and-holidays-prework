664c5da5879280a896e51719ed2a18db3ad5cc05
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-16 17:02:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,11 +65,10 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holiday|
   puts "#{season.to_s.capitalize!}:"
-  holiday.each do |holiday, supplies|
-    string_holiday = holiday.to_s.split("_").collect { |word| word.capitalize!.join(" ") 
-      puts " #{string_holiday}: #{supplies.join(", ")}"
-    end
-  end
+  holiday.each do |holiday, item|
+       puts "  #{holiday.to_s.split("_").collect {|word| word.capitalize}.join(" ")}: #{item.join(", ")}"
+     end
+  end 
 end
 
 all_supplies_in_holidays(holiday_hash)

1205e5eb3d933fafd5375f5a8adefca0d5aa26eb
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-16 16:31:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,8 +63,10 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-holiday_hash.each do |season, |
+holiday_hash.each do |season, holiday|
   puts "#{season.to_s.capitalize!}:"
+  
+  
     
 
 end

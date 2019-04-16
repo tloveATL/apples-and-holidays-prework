274c1db44b41b1cad6e135233d853d213690c1fa
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-16 16:02:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -41,7 +41,7 @@ end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   
-  holiday_hash[:fall] = { :columbus_day }
+  holiday_hash[:fall] = :columbus_day 
   
   # remember to return the updated hash
 

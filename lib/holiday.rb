6fb065a752deab80507120b2cbfd6da22f0329d7
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-16 17:09:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,6 +72,7 @@ def all_supplies_in_holidays(holiday_hash)
 
 all_supplies_in_holidays(holiday_hash)
 
+end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists

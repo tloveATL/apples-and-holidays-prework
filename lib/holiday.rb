729496a1e1f54b44aa1bf1b1eb610b0eae529290
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-16 17:04:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,7 +77,7 @@ all_supplies_in_holidays(holiday_hash)
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-holiday_hash.map do |season, holidays|
+  holiday_hash.map do |season, holidays|
     holidays.map do |holiday, items|
       if items.include?("BBQ")
         holiday
@@ -85,6 +85,8 @@ holiday_hash.map do |season, holidays|
     end
   end.flatten.compact
 end
+
+
 end
 
 

328faa9aa27514f02372a474b2f5d248efee7e7e
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-16 15:49:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,8 +18,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
-holiday_hash[:summer][:fourth_of_july][2]
-binding.pry
+holiday_hash[:summer][:fourth_of_july]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)

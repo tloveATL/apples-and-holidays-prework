7950680b3525e45e8d269b56581afa4166076848
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-16 16:31:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 holiday_hash.each do |season, holiday|
   puts "#{season.to_s.capitalize!}:"
-  holiday.each do | 
+  holiday.each do |holiday, supplies|
     
 
 end

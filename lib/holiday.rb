fd75470e3f38c91518b108504290228be7c26b19
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-16 16:48:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,6 +72,10 @@ holiday_hash.each do |season, holiday|
   end
 end
 
+all_supplies_in_holidays
+
+end
+
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"

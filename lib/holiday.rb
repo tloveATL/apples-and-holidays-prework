0fe9ff127425f3adee9d147906139072b044a962
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-16 16:40:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,6 +67,7 @@ holiday_hash.each do |season, holiday|
   puts "#{season.to_s.capitalize!}:"
   holiday.each do |holiday, supplies|
     string_holiday = holiday.to_s.split("_").collect { |word| word.capitalize.join(" ") 
+    puts 
     
 
 end
